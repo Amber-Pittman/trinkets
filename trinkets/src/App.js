430281fd9e3ffb799b7e27e0c05b8680bd3729d8
Amber-Pittman <amber@amberpittman.com>
2020-03-17 18:08:49 -0400

    Built out App and wrapped App in Router

--- trinkets/src/App.js
+++ trinkets/src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Route, NavLink } from "react-router-dom";
+import axios from "axios";
 
-function App() {
+import Home from "./components/Home";
+import ItemsList from "./components/ItemsList";
+import Item from "./components/Item";
+import ItemForm from "./components/ItemForm";
+
+import './styles.css';
+
+const App = () => {
+  //this const is called "derived state"
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/items")
+      .then(res => setItems(res.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+      <h1 className="store-header"> Dustin's Trinkets</h1>
+        <div className="nav-links">
+          <NavLink exact to="/item-form">
+            Add Item
+          </NavLink>
+
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+
+          <NavLink to="/item-list">
+            Shop
+          </NavLink>
+        </div>
+      </nav>
+
+      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/item-list"
+        render={props => <ItemsList {...props} items={items} />}
+      />
+      <Route
+        path="/item-list/:id"
+        render={props => (
+          <Item {...props} items={items} updateItems={setItems} />
+        )}
+      />
+      <Route path="/item-form" component={ItemForm} />      
+    
     </div>
   );
 }
